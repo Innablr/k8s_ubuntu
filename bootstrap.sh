bd506740c4306f9736d2b18523d6e4b57c6a391e
Simon Roberts <lyricnz@gmail.com>
2020-05-07 12:20:29 +1000

    Update to Ubuntu 20.04 LTS and use distro Docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,8 @@ EOF
 
 echo "[TASK 2] Install docker container engine"
 apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
-apt-get install docker-ce -y
+apt install docker.io -y
 
 # add ccount to the docker group
 usermod -aG docker vagrant
